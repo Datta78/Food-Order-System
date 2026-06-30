8fc3233190da97f6bd553cdf92b4269ce846e749
Dattatray Vijay Bhosale <bdattatray78@gmail.com>
2026-06-30 08:13:42 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+ <div class="footer">
+    <div class="footer-content"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61104.90356505111!2d74.60436138434109!3d16.823554296272185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc123ca0f0a504f%3A0x8790662199014c4f!2sMiraj%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1769102817768!5m2!1sen!2sin" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+    <div class="footer-content">
+        <h2>Get In to touch</h2>
+      <p><i class="fa fa-map-marker" aria-hidden="true"></i>Your address</p>
+      <p><i class="fa fa-envelope-o" aria-hidden="true"></i>abc@123</p>
+      <p><i class="fa fa-phone-square" aria-hidden="true"></i>+91 5425487875</p>
+      <p><i class="fa fa-whatsapp" aria-hidden="true"></i>+91 4878454545</p>
+
+    </div>
+   
+    <div class="footer-content">
+        <h2>Follow Us On</h2>
+    <p><i class="fa fa-check" aria-hidden="true"></i><a href="About.php">About Us</a></p>
+    <p><i class="fa fa-check" aria-hidden="true"></i><a href="food.php">Menu</p>
+    <p><i class="fa fa-check" aria-hidden="true"></i><a href="contact.php">Contact</p>
+    </div>
+</div>   
+</body>
+</html>
