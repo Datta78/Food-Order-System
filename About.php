21c0bbe2669bc431e875471dd860d8220d0e43a5
Dattatray Vijay Bhosale <bdattatray78@gmail.com>
2026-06-30 08:10:05 +0530

    Add files via upload

---
+++ About.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <?php include('menubar.php');
+?>
+<div class="container">
+    <div id="left">
+        <h1><u>About Us</u></h1>
+
+                <p>MyOnlineMeal is an online food ordering system that allows user to easily order fresh and delicious food online from multiple food vender. Customer can browser food items, add them to cart,and place order from comfert of their home.</p>
+
+        
+             <a href="food.php">
+              <button style="background-color: orange;">View Menu</button>
+            </a>
+       
+    </div>
+
+    <div class="right1">
+        <img src="images/shafe.jpeg" alt="Food Image">
+    </div>
+</div>  
+<center>
+<h2 style="size:40px">Our Team Members</h2>
+<hr>
+<Br>
+<br>
+<div class="featured-outer">
+     <div class="featured-i"><img src="images/team1.jpg"></div>
+        <div class="featured-i"><img src="images/team5.jpg"></div>
+         <div class="featured-i"><img src="images/team1.jpg"></div>
+         <div class="featured-i"><img src="images/team2.jpg"></div>
+        </div>
+</div>
+<div class="featured-outer">
+     <div class="featured-i"><img src="images/team2.jpg"></div>
+   <div class="featured-i"><img src="images/team5.jpg"></div>
+     <div class="featured-i"><img src="images/team1.jpg"></div>
+         <div class="featured-i"><img src="images/team2.jpg"></div>
+        </div>
+</div>
+</center>
+<br>
+<center>
+<h2 style="size:40px">Our Chef's</h2>
+<hr>
+<br>
+
+<Br>
+<div class="content">
+    <div class="left-content">
+        <img src="images/shafe2.jpg" alt=""  height="350px">
+    </div>
+    <div class="right-content">
+        <P>Samosa is one of the most popular traditional snacks in India. It has a crispy, golden-brown outer covering made from refined flour, which is filled with a delicious and spicy mixture of boiled potatoes, green peas, and special Indian spices. The samosa is deep-fried until it becomes crunchy and tasty. It is usually served hot with green chutney or sweet tamarind chutney. Because of its rich taste, crunchy texture, and wide popularity among people of all ages, samosa is selected as a speciality in our project.</P>
+    </div>
+</div>
+</center>
+<center>
+<Br>
+<br>
+<div class="content">
+    
+    <div class="right-content">
+        <P>Burger is a popular fast food enjoyed by people all over the world. It is made with a soft bun filled with a vegetable patty, fresh vegetables like lettuce, tomato, and onion, along with tasty sauces and cheese. The burger is assembled carefully and served hot, making it both filling and delicious. It is loved by people of all age groups because of its rich taste and attractive presentation. Due to its popularity and delicious flavor, burger is selected as a speciality in our project.</P>
+    </div>
+    <div class="left-content">
+        <img src="images/shafe1.jpg" alt=""  height="280px">
+    </div>
+</div>
+</center>
+<center>
+<Br>
+<br>
+<div class="content">
+    <div class="left-content">
+        <img src="images/shafe.jpeg" alt=""  height="350px">
+    </div>
+    <div class="right-content">
+        <P>Pizza is one of the most popular and widely enjoyed fast foods around the world. It consists of a soft or crispy base made from refined flour, topped with tomato sauce, cheese, and a variety of vegetables or other toppings. The pizza is baked in an oven until the cheese melts and the base becomes crispy and flavorful. It is usually served hot and is loved by people of all age groups. Because of its delicious taste, attractive appearance, and great popularity, pizza is selected as a speciality in our project.</P>
+    </div>
+</div>
+</center>
+<?php include("footer.php"); ?>
+</center>
+</body>
+</html>
