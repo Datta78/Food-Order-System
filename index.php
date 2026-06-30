8fe0ab5238d4a5045f5c39f91c261c76278e51e6
Dattatray Vijay Bhosale <bdattatray78@gmail.com>
2026-06-30 08:14:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Best Online Food Delivery | MyOnlineMeal</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<?php include('menubar.php');
+?>
+
+<div class="container">
+    <div id="left">
+        <h1>Welcome to <u>MyOnlineMeal</u></h1>
+
+        <p>MyOnlineMeal is an online food ordering system that allows user to easily order fresh and delicious food online from multiple food vender. Customer can browser food items, add them to cart,and place order from comfert of their home.</p>
+        <p>MyOnlineMeal is an online food ordering system that allows user to easily order fresh and delicious food online from multiple food vender. Customer can browser food items, add them to cart,and place order from comfert of their home.</p>
+<br>
+        <a href="food.php" style="background-color:orange; padding:15px; text-decoration:none; color:black; border-radius:5px;">
+View More →
+</a>
+       
+    </div>
+
+    <div class="right">
+        <img src="images/Hero3.jpeg" alt="Food Image">
+    </div>
+</div>
+<center>
+<h2 style="size:40px">WHY CHOOSE US ?</h2>
+<hr>
+<Br>
+<br>
+<div class="featured-outer">
+     <div class="featured-icon"><i class="fa fa-cutlery" aria-hidden="true" style="color: orange;"></i>
+          <br>  <p><u>Fersh Food</u>
+            <p>We prepare a fresh hygienic food every day. Only quality ingredients are used to ensure great taste and health.</p>
+          </p>
+        </div>
+         <div class="featured-icon"><i class="fa fa-clock-o" aria-hidden="true" style="color: green;"></i><br>
+        <p><u>24x7</u>
+            <p>Our survice is available 24/7 for your convenience. Order any time and enjoy uniterrupted support.</p>
+        </p>
+        </div>
+         <div class="featured-icon"><i class="fa fa-motorcycle" aria-hidden="true" style="color: orange;"></i><br>
+        <p><u>Fast Delivery</u>
+           <p>We are provide quick and reliable delivery. Your food reaches you fresh,hot,and on time.</p> 
+        </p>
+         </div>
+         <div class="featured-icon"><i class="fa fa-star" aria-hidden="true" style="color: orange;"></i><br>
+            <p><u>259 reviews</u>
+                <p>Trusted by many customers. Our quality service is reflected in positive reviews.</p>
+            </p>
+        </div>
+
+</center>
+<center>
+<h2 style="size:40px">Our speciality</h2>
+<hr>
+<Br>
+<div class="content">
+    <div class="left-content">
+        <img src="images/samosa1.jpg" alt="">
+    </div>
+    <div class="right-content">
+        <P>Samosa is one of the most popular traditional snacks in India. It has a crispy, golden-brown outer covering made from refined flour, which is filled with a delicious and spicy mixture of boiled potatoes, green peas, and special Indian spices. The samosa is deep-fried until it becomes crunchy and tasty. It is usually served hot with green chutney or sweet tamarind chutney. Because of its rich taste, crunchy texture, and wide popularity among people of all ages, samosa is selected as a speciality in our project.</P>
+    </div>
+</div>
+</center>
+<center>
+<Br>
+<br>
+<div class="content">
+    
+    <div class="right-content" >
+        <P>Burger is a popular fast food enjoyed by people all over the world. It is made with a soft bun filled with a vegetable patty, fresh vegetables like lettuce, tomato, and onion, along with tasty sauces and cheese. The burger is assembled carefully and served hot, making it both filling and delicious. It is loved by people of all age groups because of its rich taste and attractive presentation. Due to its popularity and delicious flavor, burger is selected as a speciality in our project.</P>
+    </div>
+    <div class="left-content">
+        <img src="images/burger.jpg" alt=""   height="350px">
+    </div>
+</div>
+</center>
+<center>
+<Br>
+<br>
+<div class="content">
+    <div class="left-content">
+        <img src="images/pizza.jpg" alt=""  height="350px">
+    </div>
+    <div class="right-content">
+        <P>Pizza is one of the most popular and widely enjoyed fast foods around the world. It consists of a soft or crispy base made from refined flour, topped with tomato sauce, cheese, and a variety of vegetables or other toppings. The pizza is baked in an oven until the cheese melts and the base becomes crispy and flavorful. It is usually served hot and is loved by people of all age groups. Because of its delicious taste, attractive appearance, and great popularity, pizza is selected as a speciality in our project.</P>
+    </div>
+</div>
+</center>
+<br>
+<br>
+<br>
+
+<?php include("footer.php"); ?>
+</center>
+</body>
+</html>
